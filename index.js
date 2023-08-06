08dde663cafd4119386ecfedd908444732fdc546
RalucaPoenar <130293342+RalucaPoenar@users.noreply.github.com>
2023-08-06 17:45:33 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+
+
+    console.log('Username:', username);
+    console.log('Password:', password);
+}
+
+
+function loginWithGoogle() {
+    alert('Redirect to Google authentication...');
+}
+
+function loginWithFacebook() {
+    alert('Redirect to Facebook authentication...');
+}
+
+function loginWithInstagram() {
+    alert('Redirect to Instagram authentication...');
+}
+
+
+
+
